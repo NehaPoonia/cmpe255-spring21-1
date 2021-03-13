a52836c1531bc7466b3082ef26c0ec8b4e139983
Nea <Nea@rahuls-MacBook-Pro.local>
2021-03-12 22:46:02 -0800

    Lab 2

--- lab2/predict-car-price.py
+++ lab2/predict-car-price.py
@@ -16,8 +16,107 @@ class CarPrice:
         for col in string_columns:
             self.df[col] = self.df[col].str.lower().str.replace(' ', '_')
 
-    def validate(self):
-        pass
+    def linear_regression(self, X, y, r=1):
+        ones = np.ones(X.shape[0])
+        X = np.column_stack([ones, X])
 
-    def linear_regression(self, X, y):
-        pass
+        XTX = X.T.dot(X)
+       
+        XTX = XTX + (r * np.eye(XTX.shape[0]))
+       
+        XTX_inv = np.linalg.inv(XTX)
+        w = XTX_inv.dot(X.T).dot(y)
+   
+        return w[0], w[1:]
+
+    def prepare_X(self, df):
+        base = ['engine_hp', 'engine_cylinders', 'highway_mpg', 'city_mpg', 'popularity']
+        df_num = df[base]
+        df_num = df_num.fillna(0)
+        X = df_num.values
+        return X
+
+    def rmse(self, y, y_pred):
+        error = y_pred - y
+        mse = (error ** 2).mean()
+        return np.sqrt(mse)
+   
+    def validate(self):
+
+        np.random.seed(2)
+        n = len(self.df)
+        n_val = int(0.2 * n)
+        n_test = int(0.2 * n)
+        n_train = n - (n_val + n_test)
+       
+        # creates an array of range n
+        idx = np.arange(n)
+        np.random.shuffle(idx)
+       
+        # shuffles the ids of the dataframe
+        df_shuffled = self.df.iloc[idx]
+       
+        df_train = df_shuffled.iloc[:n_train].copy()
+        df_val = df_shuffled.iloc[n_train:n_train+n_val].copy()
+        df_test = df_shuffled.iloc[n_train+n_val:].copy()
+       
+        y_train_orig = df_train.msrp.values
+        y_val_orig = df_val.msrp.values
+        y_test_orig = df_test.msrp.values
+
+        y_train = np.log1p(df_train.msrp.values)
+        y_val = np.log1p(df_val.msrp.values)
+        y_test = np.log1p(df_test.msrp.values)
+
+        del df_train['msrp']
+        del df_val['msrp']
+        del df_test['msrp']
+       
+       
+        # training dataset
+        X_train = self.prepare_X(df_train)
+        # validation dataset
+        X_val = self.prepare_X(df_val)
+       
+        for r in [0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10]:
+            w_0, w = self.linear_regression(X_train, y_train, r)
+            y_pred = w_0 + X_train.dot(w)
+            y_val_pred = w_0 + X_val.dot(w)
+            # print(r, self.rmse(y_train, y_pred))
+            # print(r, self.rmse(y_val, y_val_pred))
+   
+        # Got minimum rmse at r = 1 for validation data
+        w_0, w = self.linear_regression(X_train, y_train)
+       
+        # test dataset
+        X_test = self.prepare_X(df_test)
+        y_test_pred = w_0 + X_test.dot(w)
+       
+        y_val_pred = np.expm1(y_val_pred)
+        y_test_pred = np.expm1(y_test_pred)
+
+        # self.display(df_val, y_val_orig, y_val_pred)
+        self.display(df_test, y_test_orig, y_test_pred)
+       
+       
+    def display(self, df, y, y_pred):
+        df['msrp'] = y
+        df['msrp_pred'] = y_pred
+        cols = [
+            'engine_cylinders', 'transmission_type', 'driven_wheels',
+            'number_of_doors', 'market_category', 'vehicle_size',
+            'vehicle_style', 'highway_mpg', 'city_mpg',
+            'popularity', 'msrp', 'msrp_pred'
+        ]
+        print(df[cols].head().to_string(index=False))
+       
+
+
+def main():
+    carprice = CarPrice()
+   
+    carprice.trim()
+    carprice.validate()
+
+if __name__ == "__main__":
+    main()
